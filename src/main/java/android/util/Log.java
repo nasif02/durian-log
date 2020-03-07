953830bc0f850142dd46c6f1d57127a3f3ee9f37
xplo <nasif.002@gmail.com>
2020-03-07 11:11:56 +0600

    write Log class

---
+++ src/main/java/android/util/Log.java
@@ -0,0 +1,17 @@
+package android.util;
+
+public class Log {
+
+    public static void d(String tag, String msg) {
+        System.out.println(tag + " : " + msg);
+    }
+
+    public static void e(String tag, String msg) {
+        System.err.println(tag + " : " + msg);
+    }
+
+    public static void i(String tag, String msg) {
+        System.out.println(tag + " : " + msg);
+    }
+
+}
